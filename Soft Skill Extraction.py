03fadb53d15bf6b3749cffc6da1c189c5141a623
Gozie02 <83558859+Gozie02@users.noreply.github.com>
2022-05-29 18:51:42 +0100

    Delete Soft Skill Extraction.py

--- Soft Skill Extraction.py
+++
@@ -1,9 +0,0 @@
-from Soft Skills import soft_skills
-import spacy
-nlp = spacy.load('en_core_web_sm')
-from spacy.matcher import Matcher
-import pandas as pd
-df = pd.read_csv(r"C:/Users/uchei/OneDrive/Desktop/Raw_Skills_Dataset.csv")
-df2['Soft Skills'] = df['RAW DATA'].apply(lambda x:soft_skills(x))
-#eliminate all empty values
-df2 = df2[df2.apply(len) > 0]
